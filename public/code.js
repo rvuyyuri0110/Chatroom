339a66ac7a591f2be2d8c9ad73a2b524481428e9
rvuyyuri0110 <raghuvuyyuri206@gmail.com>
2024-07-20 12:54:17 +0530

    second commit

--- public/code.js
+++ public/code.js
@@ -1,76 +1,114 @@
-(function(){
+(function () {
     const app = document.querySelector(".app");
     const socket = io();
 
-    let uname; 
+    let uname;
+    let room;
 
-    app.querySelector(".join-screen #join-user").addEventListener("click",function(){
-        let username = app.querySelector(".join-screen #username").value;
-        if(username.length == 0){
+    // Event listener for creating a room
+    app.querySelector("#create-room").addEventListener("click", function () {
+        uname = app.querySelector("#username").value;
+        if (uname.length === 0) {
             return;
         }
-        socket.emit("newuser",username);
-        uname = username;
-        app.querySelector(".join-screen").classList.remove("active");
+        room = Math.floor(100000 + Math.random() * 900000).toString();
+        socket.emit("joinRoom", { username: uname, room: room });
+        app.querySelector(".join-create-screen").classList.remove("active");
         app.querySelector(".chat-screen").classList.add("active");
-
+        renderMessage("update", `Room created: ${room}`);
     });
-    app.querySelector(".chat-screen #send-message").addEventListener("click",function (){
-        let message = app.querySelector(".chat-screen #message-input").value;
-        if (message.length == 0){
+
+    // Event listener to show join room input
+    app.querySelector("#join-room-btn").addEventListener("click", function () {
+        app.querySelector(".join-room-input").style.display = "block";
+    });
+
+    // Event listener for joining a room
+    app.querySelector("#join-room").addEventListener("click", function () {
+        uname = app.querySelector("#username").value;
+        room = app.querySelector("#room-code").value;
+        if (uname.length === 0 || room.length === 0) {
             return;
         }
-        renderMessage("my",
-            {
-                username:uname,
-                text:message
-            }
-        );
-        socket.emit("chat",{
-            username: uname,
-            text:message
-        });
-        app.querySelector(".chat-screen #message-input").value = "";
+        socket.emit("joinRoom", { username: uname, room: room });
+        app.querySelector(".join-create-screen").classList.remove("active");
+        app.querySelector(".chat-screen").classList.add("active");
     });
 
-    app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
-        socket.emit("exituser",uname);
+    // Event listener for sending a message
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function () {
+        sendMessage();
+    });
+
+    // Event listener for sending a message on Enter key press
+    app.querySelector(".chat-screen #message-input").addEventListener("keypress", function (e) {
+        if (e.key === "Enter") {
+            sendMessage();
+        }
+    });
+
+    // Event listener for exiting the chat
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click", function () {
+        socket.emit("exituser", uname);
         window.location.href = window.location.href;
     });
-    socket.on("update",function(update){
-        renderMessage("update",update);
+
+    // Socket listener for update messages
+    socket.on("update", function (update) {
+        renderMessage("update", update);
     });
-    socket.on("chat",function(message){
-        renderMessage("other",message);
+
+    // Socket listener for chat messages
+    socket.on("chat", function (message) {
+        renderMessage("other", message);
     });
-    function renderMessage(type,message){
+
+    // Function to send a message
+    function sendMessage() {
+        let message = app.querySelector(".chat-screen #message-input").value;
+        if (message.length === 0) {
+            return;
+        }
+        renderMessage("my", {
+            username: uname,
+            text: message
+        });
+        socket.emit("chat", {
+            username: uname,
+            text: message
+        });
+        app.querySelector(".chat-screen #message-input").value = "";
+    }
+
+    // Function to render messages on the screen
+    function renderMessage(type, message) {
         let messageContainer = app.querySelector(".chat-screen .messages");
-        if (type== "my"){
+        if (type === "my") {
             let el = document.createElement("div");
-            el.setAttribute("class","message my-message");
+            el.setAttribute("class", "message my-message");
             el.innerHTML = `
-            <div>
-                <div class="name>You</div>
-                <div class="text>${message.text}</div>
-            </div>
+                <div>
+                    <div class="name">You</div>
+                    <div class="text">${message.text}</div>
+                </div>
             `;
             messageContainer.appendChild(el);
-        }else if (type == "other"){
+        } else if (type === "other") {
             let el = document.createElement("div");
-            el.setAttribute("class","message other-message");
+            el.setAttribute("class", "message other-message");
             el.innerHTML = `
-            <div>
-                <div class="name>${message.username}</div>
-                <div class="text>${message.text}</div>
-            </div>
+                <div>
+                    <div class="name">${message.username}</div>
+                    <div class="text">${message.text}</div>
+                </div>
             `;
             messageContainer.appendChild(el);
-        }else if (type == "update"){
+        } else if (type === "update") {
             let el = document.createElement("div");
-            el.setAttribute("class","update");
+            el.setAttribute("class", "update");
             el.innerText = message;
             messageContainer.appendChild(el);
-        } 
+        }
         messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
 })();
--- server.js
+++ server.js
@@ -4,18 +4,35 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
-const io = require ("socket.io")(server);
+const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname+"/public")));
-io.on("connection",function(socket){
-    socket.on("newuser",function(username){
-        socket.broadcast.emit("update",username + " joined the conversation");   
+app.use(express.static(path.join(__dirname, "/public")));
+
+io.on("connection", function (socket) {
+    socket.on("joinRoom", function (data) {
+        socket.join(data.room);
+        socket.room = data.room;
+        socket.username = data.username;
+        socket.broadcast.to(data.room).emit("update", `${data.username} joined the room.`);
     });
-    socket.on("exituser",function(username){
-        socket.broadcast.emit("update",username + " left the conversation");
+
+    socket.on("newuser", function (data) {
+        socket.join(data.room);
+        socket.room = data.room;
+        socket.username = data.username;
+        socket.broadcast.to(data.room).emit("update", `${data.username} joined the conversation`);
     });
-    socket.on("chat",function(message){
-        socket.broadcast.emit("chat",message);
+
+    socket.on("exituser", function (username) {
+        socket.broadcast.to(socket.room).emit("update", `${username} left the conversation`);
+        socket.leave(socket.room);
+    });
+
+    socket.on("chat", function (message) {
+        socket.broadcast.to(socket.room).emit("chat", message);
     });
 });
-server.listen(5000);
+
+server.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
